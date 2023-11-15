50a1f795c4727a7dddaf64a0a9d504196bdec366
Pedro Camacho <pedrohcamacho@hotmail.com>
2023-11-15 15:04:59 -0300

    Adicionando as fontes e configurando o arquivo de estilo

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome} from '@fortawesome/free-solid-svg-icons'
+import { theme } from '../Styles/theme';
 
 function Home () {
-    
     return (
         <div>
             <h1 style={styles.title}>Seja bem vindo!</h1>
+            <FontAwesomeIcon icon={faHome} style={styles.icon}/>
         </div>  
     )
 }
 
 const styles = {
-    // title:{
-    //     fontFamily: theme.fonts.title,
-    //     color: theme.colors.title,
-    //     marginTop: "2%"
-    // },
+    title:{
+        fontFamily: theme.fonts.bold,
+        color: theme.colors.primary,
+        marginTop: "2%"
+    },
 }
 
 export default Home;
---
+++ src/Styles/theme.js
@@ -0,0 +1,23 @@
+export const theme = {
+    colors: {
+        primary: "#232323",
+        secondary: "#FDA412",
+        card2:"#793838",
+        card3:"#3177A3"
+    },
+  
+    fonts: {
+      title: "'Inter', sans-serif", 
+      bold: "'Montserrat', sans-serif",
+      text: "'PT Sans', sans-serif",
+      black: "'Roboto', sans-serif"
+    },
+    FONT_SIZE: {
+      SM: 14,
+      MD: 16,
+      LG: 28
+    }
+  };
+
+
+  
