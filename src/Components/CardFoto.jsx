d001b8a80a7348ee908e75a1f0358b6f3f231aa8
Pedro Camacho <pedrohcamacho@hotmail.com>
2023-11-15 20:09:26 -0300

    Criei o componente de card com a foto e coloquei na página Home

---
+++ src/Components/CardFoto.jsx
@@ -0,0 +1,62 @@
+import { theme } from '../Styles/theme'
+import Caminhao from '../assets/caminhao.png';
+
+function CardFoto() {  
+ 
+  return (
+    <div style={styles.content}>
+        <div style={{flexDirection:"column"}}>
+          <h1 style={styles.title}>Frotas Cadastradas</h1>
+          <a style={styles.Button}>Consultar</a>
+        </div>
+        <div>
+            <img src={Caminhao} alt="" style={styles.imageStyle}/>
+        </div>
+    </div>
+  )
+    
+}
+
+const styles = {
+  content:{
+    display:"flex",
+    justifyContent:"space-around",
+    width: 800,
+    height: 280,
+    backgroundColor: theme.colors.primary,
+    dropShadow: '(5px 5px 5px 5px black)',
+    borderRadius: "10px"
+  },
+  title:{
+    fontFamily: theme.fonts.text,
+    color: theme.colors.sidebarText,
+    marginTop:"7%",
+    paddingBottom:"15%",
+    fontWeight: 700
+},
+Button: {
+  width: 200,
+  height: "45px",
+  borderRadius: "5px",
+  textAlign: "center",
+  border: "0px solid",
+  display: "flex",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+  fontFamily: theme.fonts.text,
+  color:theme.colors.secondary,
+  backgroundColor:theme.colors.sidebarText,
+  fontSize: "17px",
+  fontWeight: 700,
+  cursor:"pointer",
+  textDecoration:"none"
+},
+  imageStyle:{
+    width: 370,
+    height: 280,
+    
+  }
+}
+
+export default CardFoto;
+
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import { theme } from '../Styles/theme';
-
+import CardFoto from '../Components/CardFoto';
 
 function Home () {
 
     return (
         <div>
             <h1 style={styles.title}>Seja bem vindo!</h1>
+           
+            <div style={styles.card}>
+                <CardFoto/>
+            </div> 
         </div>  
         
     )
@@ -18,6 +22,9 @@ const styles = {
         marginTop: "2%",
         fontWeight: 800
     },
+    card:{
+        marginTop:"3%"
+    }
 }
 
 export default Home;
